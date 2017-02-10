5cf4eef7f50a27e522d9947103012cf7a1f62701
Gabe Escobedo <escobedo.gabe@yahoo.com>
2017-02-10 03:58:48 -0600

    cant use single quotes on C#

---
+++ CSharp/chapter_02/program_0202.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program_0202
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kate Austen");
+            Console.WriteLine("123 Full Circle Drive");
+            Console.WriteLine("Asheville, NC 28899");
+            Console.ReadLine();
+        }
+    }
+}
