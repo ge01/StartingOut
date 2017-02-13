c80b42df37e725ed61e10f0427cc6911021898a6
Gabe Escobedo <gabriel.escobedo01@utrgv.edu>
2017-02-13 07:45:37 -0600

    using double quotes

---
+++ CSharp/chapter_02/program_0203.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program_0203
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dont't fear!");
+            Console.WriteLine("I'm here!");
+            Console.ReadLine();
+        }
+    }
+}
