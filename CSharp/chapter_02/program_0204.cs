2a309a5e8690bf8acade81fe060824f4a4c57ae5
Gabe Escobedo <gabriel.escobedo01@utrgv.edu>
2017-02-20 22:37:48 -0600

    how to include quotes in a C# string

---
+++ CSharp/chapter_02/program_0204.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program_0204
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Your assignment is to read \"Hamlet\" by tomorrow.");
+            Console.ReadLine();
+        }
+    }
+}
