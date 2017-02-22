8fc19de5e705fc07eebd249fc6f1764e0b366c52
Gabe Escobedo <gabriel.escobedo01@utrgv.edu>
2017-02-21 23:32:06 -0600

    C# - display multiple items with one call to the Console.WriteLine method

---
+++ CSharp/chapter_02/program_0209/program_0209/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program_0209
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This program demonstrates a variable.
+            var room = 503;
+            Console.WriteLine("I am staying in room number {0}", room);
+            Console.ReadLine();
+        }
+    }
+}
