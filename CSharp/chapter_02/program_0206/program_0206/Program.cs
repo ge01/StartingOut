99d503620932a9974b44c8b25c1aecdd1c1fc708
Gabe Escobedo <gabriel.escobedo01@utrgv.edu>
2017-02-21 21:49:07 -0600

    C# end-line comments

---
+++ CSharp/chapter_02/program_0206/program_0206/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program_0206
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kate Austen");               // Display the name.
+            Console.WriteLine("123 Full Circle Drive");     // Display the address.
+            Console.WriteLine("Asheville, NC 28899");       // Display the city, state, and ZIP.
+            Console.ReadLine();
+        }
+    }
+}
