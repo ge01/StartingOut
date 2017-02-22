755f4667d666a3e99c098582c03a275aa7d491f8
Gabe Escobedo <gabriel.escobedo01@utrgv.edu>
2017-02-22 03:34:43 -0600

    C# - storing strings

---
+++ CSharp/chapter_02/program_0211/program_0211/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program_0211
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create variables to reference two strings.
+            string firstName = "Kathryn";
+            var lastName = "Marino";
+
+            // Display the values referenced by the variables.
+            Console.WriteLine("{0} {1}", firstName, lastName);
+
+            Console.ReadLine();
+        }
+    }
+}
