1c6626a25f406f2c8b14a8d626d02395555246ce
Gabe Escobedo <gabriel.escobedo01@utrgv.edu>
2017-02-21 21:30:56 -0600

    C# comments

---
+++ CSharp/chapter_02/program_0205/program_0205/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program_0205
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This program displays a person's
+            // name and address. 
+            Console.WriteLine("Kate Austen");
+            Console.WriteLine("123 Full Circle Drive");
+            Console.WriteLine("Asheville, NC 28899");
+            Console.ReadLine();
+        }
+    }
+}
