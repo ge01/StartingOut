76bb083e64b17dc3a8a3fe25e7effb8fbaf76a3c
Gabe Escobedo <gabriel.escobedo01@utrgv.edu>
2017-02-21 22:23:29 -0600

    create a variable named room and assign it the value 503 - C#

---
+++ CSharp/chapter_02/program_0207/program_0207/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program_0207
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This program demonstrates a variable.
+            var room = 503;
+            Console.WriteLine("I am staying in room number");
+            Console.WriteLine(room);
+            Console.ReadLine();
+        }
+    }
+}
