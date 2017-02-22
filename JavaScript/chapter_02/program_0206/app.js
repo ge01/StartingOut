2236bd86a789b636ae8d152d9de69a7c9cfb64aa
Gabe Escobedo <gabriel.escobedo01@utrgv.edu>
2017-02-21 21:45:04 -0600

    javascript end-line comments

---
+++ JavaScript/chapter_02/program_0206/app.js
@@ -0,0 +1,4 @@
+
+console.log('Kate Austen');               // Display the name.
+console.log('123 Full Circle Drive');     // Display the address.
+console.log('Asheville, NC 28899');       // Display the city, state, and ZIP
