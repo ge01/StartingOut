758852518627922ceaee98ffe59fa68e08330549
Gabe Escobedo <gabriel.escobedo01@utrgv.edu>
2017-02-21 23:00:57 -0600

    C# - simple program that uses two variables

---
+++ CSharp/chapter_02/program_0208/program_0208/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program_0208
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create two variables: top_speed and distance
+            int top_speed = 160;
+            var distance = 300;
+
+            // Display the values referenced by the variables.
+            Console.WriteLine("The top speed is");
+            Console.WriteLine(top_speed);
+            Console.WriteLine("The distance traveled is");
+            Console.WriteLine(distance);
+            Console.ReadLine();
+        }
+    }
+}
