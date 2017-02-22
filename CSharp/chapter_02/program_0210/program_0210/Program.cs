70f1a7f62319189063f494d3442a35a46ece23c3
Gabe Escobedo <gabriel.escobedo01@utrgv.edu>
2017-02-22 02:55:44 -0600

    C# - variable reassignment

---
+++ CSharp/chapter_02/program_0210/program_0210/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program_0210
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This program demonstrates variable reassignment.
+            // Assign a value to the dollars variable.
+            var dollars = 2.75;
+            Console.WriteLine("I have {0} in my account.", dollars);
+
+            // Reassign dollars so it references
+            // a different value.
+            dollars = 99.95;
+            Console.WriteLine("I have {0} in my account.", dollars);
+
+            Console.ReadLine();
+        }
+    }
+}
