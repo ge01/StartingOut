45aa924d1d5954e8639a31053a1afc6278a7c534
Gabe Escobedo <gabriel.escobedo01@utrgv.edu>
2017-02-21 22:55:19 -0600

    JS - simple program that uses two variables

---
+++ JavaScript/chapter_02/program_0208/app.js
@@ -0,0 +1,9 @@
+// Create two variables: top_speed and distance.
+top_speed = 160;
+distance = 300;
+
+// Display the values referenced by the variables.
+console.log('The top speed is');
+console.log(top_speed);
+console.log('The distance traveled is');
+console.log(distance);
