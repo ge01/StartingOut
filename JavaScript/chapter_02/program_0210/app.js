2d6c896894298c74bdc1e6044a9ae1d8fbfff154
Gabe Escobedo <gabriel.escobedo01@utrgv.edu>
2017-02-22 02:44:59 -0600

    JS - variable reassignment

---
+++ JavaScript/chapter_02/program_0210/app.js
@@ -0,0 +1,9 @@
+// This program demonstrates variable reassignment.
+// Assign a value to the dollars variable.
+var dollars = 2.75;
+console.log('I have', dollars, 'in my account.');
+
+// Reassign dollars so it references
+// a different value.
+dollars = 99.95;
+console.log('But now I have', dollars, 'in my account.');
