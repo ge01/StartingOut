f18c80b355469a0819905b0359442d53af630372
Gabe Escobedo <gabriel.escobedo01@utrgv.edu>
2017-02-23 20:32:16 -0600

    JS - using prompt with parseInt and parseFloat to enter data

---
+++ JavaScript/chapter_02/program_0213/app.js
@@ -0,0 +1,10 @@
+// Get the user's name, age, and income.
+var name = prompt('What is your name?');
+var age = parseInt(prompt('What is your age'));
+var income = parseFloat(prompt('What is your income?'));
+
+// Display the data.
+console.log('Here is the data you entered:');
+console.log('Name: ' + name);
+console.log('Age:', age);
+console.log('Income: ' + income);
