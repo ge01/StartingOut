88da9ae95709bf2e82a37ecc446f17bbfcf011a1
Gabe Escobedo <gabriel.escobedo01@utrgv.edu>
2017-02-23 18:55:11 -0600

    C# reading input from the keyboard

---
+++ CSharp/chapter_02/program_0212/program_0212/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program_0212
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get the user's first name.
+            Console.WriteLine("Enter your first name: ");
+            var firstName = Console.ReadLine();
+
+            // Get the user's last name.
+            Console.WriteLine("Enter your last name: ");
+            var lastName = Console.ReadLine();
+
+            // Print a greeting to the user.
+            Console.WriteLine("Hello " + firstName + " " + lastName);
+            Console.WriteLine();
+
+            // Alternate code to print a greeting to the user.
+            Console.WriteLine("Hello {0} {1}", firstName, lastName);
+            Console.ReadLine();
+        }
+    }
+}
