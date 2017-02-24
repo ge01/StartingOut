6a4ae34afed2fb8b83738d3c385baf7e3b2e4035
Gabe Escobedo <gabriel.escobedo01@utrgv.edu>
2017-02-23 21:09:14 -0600

    C# - using Convert.ToUInt32() and Convert.ToSingle() to convert entered data

---
+++ CSharp/chapter_02/program_0213/program_0213/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program_0213
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get the user's name, age, and income.
+            Console.Write("What is your name? ");
+            var name = Console.ReadLine();
+
+            Console.Write("What is your age? ");
+            var age = Convert.ToUInt32(Console.ReadLine());
+
+            Console.Write("What is your income? ");
+            var income = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine();
+
+            // Display the data.
+            Console.WriteLine("Here is the data you entered:");
+            Console.WriteLine("Name {0}", name);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Income: {0}", income);
+
+            Console.ReadLine();
+        }
+    }
+}
