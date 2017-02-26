a0bda98441c8929427542eb7971e9c5c05f11703
Gabe Escobedo <gabriel.escobedo01@utrgv.edu>
2017-02-26 07:48:17 -0600

    C# - performing calculations

---
+++ CSharp/chapter_02/program_0214/program_0214/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program_0214
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Assign a value to the salary
+            int salary = 2500;
+
+            // Assign a value to the bonus variable.
+            double bonus = 1200.0;
+
+            // Calculate the total pay by adding salary
+            // and bonus. Assign the result to pay.
+            var pay = salary + bonus;
+
+            // Display the pay.
+            Console.WriteLine("Your pay is " + pay);
+
+            // Alternate display the pay
+            Console.WriteLine("Your pay is {0}", pay);
+
+            Console.ReadLine();
+        }
+    }
+}
