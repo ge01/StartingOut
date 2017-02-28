902725ff08aad992928d9cb7ff7325cffbbd60b0
Gabe Escobedo <gabriel.escobedo01@utrgv.edu>
2017-02-28 08:01:27 -0600

    C# - calculating a percentage

---
+++ CSharp/chapter_02/program_0215/program_0215/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program_0215
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This program gets an item's original price and
+            // calculates its sale price, with a 20% discount
+
+            // Get the item's original price.
+            Console.Write("Enter the item's original price: ");
+            float originalPrice = Convert.ToSingle(Console.ReadLine());
+
+            // Calculate the amount of the discount.
+            double discount = originalPrice * 0.2;
+
+            // Calculate the sale price.
+            double salePrice = originalPrice - discount;
+
+            // Display the sale price.
+            Console.WriteLine("The sale price is {0}", salePrice.ToString("C2"));
+
+            Console.ReadLine();
+
+        }
+    }
+}
