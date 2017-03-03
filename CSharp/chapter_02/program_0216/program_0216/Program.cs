f610c93df659c6fc9ef80aa7062036beeceeff25
Gabe Escobedo <gabriel.escobedo01@utrgv.edu>
2017-03-02 19:35:04 -0600

    C# - calculating an average

---
+++ CSharp/chapter_02/program_0216/program_0216/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program_0216
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get three test scores and assign them to the 
+            // test1, test2, and test3 variables.
+            Console.Write("Enter the first test score: ");
+            float test1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Enter the second test score: ");
+            float test2 = float.Parse(Console.ReadLine());
+
+            Console.Write("Enter the third test score: ");
+            float test3 = float.Parse(Console.ReadLine());
+
+            // Calculate the average of the three scores
+            // and assign the result to the average variable.
+            var average = (test1 + test2 + test3) / 3.0;
+
+            // Display the average.
+            Console.WriteLine();
+            Console.WriteLine("The average score is " + average.ToString("F2"));
+            Console.ReadLine();
+        }
+    }
+}
