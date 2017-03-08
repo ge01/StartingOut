76b7ff0f3b39c64ab8b25f6ef78b8ec5d1bd0047
Gabe Escobedo <gabriel.escobedo01@utrgv.edu>
2017-03-08 03:37:34 -0600

    C# - monthlyPayment.ToString(F2)

---
+++ CSharp/chapter_02/program_0220/program_0220/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program_0220
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This program demonstrates how a floating-point
+            // number is displayed with no formatting.
+            var amountDue = 5000.0;
+            var monthlyPayment = amountDue / 12.0;
+            Console.WriteLine("The monthly payment is " + monthlyPayment.ToString("F2"));
+            Console.ReadLine();
+        }
+    }
+}
