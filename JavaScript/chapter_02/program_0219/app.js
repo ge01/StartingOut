5165373dc224aa71cb34a1b53758856bf35d59dd
Gabe Escobedo <gabriel.escobedo01@utrgv.edu>
2017-03-08 02:45:31 -0600

    JS - demonstrate how a floating-point number is displayed with no formatting

---
+++ JavaScript/chapter_02/program_0219/app.js
@@ -0,0 +1,5 @@
+// This program demonstrates how a floating-point
+// number is displayed with no formatting.
+var amountDue = 5000.0;
+var monthlyPayment = amountDue / 12.0;
+console.log('The monthly payment is ' + monthlyPayment);
