7884b2ddecd6cf96e2a73dc4e4b81a3d82b41085
Gabe Escobedo <gabriel.escobedo01@utrgv.edu>
2017-03-08 01:21:43 -0600

    C# - converting a math formula to a programming statement

---
+++ CSharp/chapter_02/program_0218/program_0218/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program_0218
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get the desired future value.
+            Console.Write("Enter the desired future value: ");
+            var futureValue = float.Parse(Console.ReadLine());
+
+            // Get the annual interest rate.
+            Console.Write("Enter the annual interest rate: ");
+            var rate = float.Parse(Console.ReadLine());
+
+            // Get the number of years that the money will appreciate.
+            Console.Write("Enter the number of years the money will grow: ");
+            var years = int.Parse((Console.ReadLine()));
+
+            // Calculate the amount needed to deposit.
+            var presentValue = futureValue / Math.Pow((1.0 + rate), years);
+
+            // Display the amount needed to deposit
+            Console.WriteLine();
+            Console.WriteLine("You will need to deposit this amount: {0}", presentValue);
+            Console.ReadLine();
+        }
+    }
+}
