633f55ef9f54add42161fe03200a765fbca0b41c
Gabe Escobedo <gabriel.escobedo01@utrgv.edu>
2017-03-12 08:38:57 -0500

    PY - inserting comma and item separators to format numbers

---
+++ Python/chapter_02/program_0221.py
@@ -0,0 +1,11 @@
+# This program demonstrates how a floating-point
+# number can be displayed as currency.
+monthly_pay = 5000.0
+annual_pay = monthly_pay * 12
+print('Your annual pay is $',\
+       format(annual_pay, ',.2f'), \
+       sep='')
+
+# Without specifying an item separator
+print('Your annual pay is $',\
+       format(annual_pay, ',.2f'))
