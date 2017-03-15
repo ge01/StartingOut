b35b599c8fbacd799a9d3bb7053f329f830fb6f1
Gabe Escobedo <gabriel.escobedo01@utrgv.edu>
2017-03-15 11:29:40 -0500

    C# - using annualPay.ToString(C2) to display number as currency

---
+++ CSharp/chapter_02/program_0221/program_0221/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program_0221
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This program demonstrates how a floating-point
+            // number can be displayed as currency.
+            var monthlyPay = 5000.0;
+            var annualPay = monthlyPay * 12;
+            Console.WriteLine("Your annual pay is " + annualPay.ToString("C2"));
+            Console.ReadLine();
+
+            // Displays "Your annual pay is $60,000.00"
+        }
+    }
+}
