b661ee2930912cd500a7f41c3cee0dcff8b85e09
Gabe Escobedo <gabriel.escobedo01@utrgv.edu>
2017-03-15 21:19:38 -0500

    JS - display each number with 2 decimal places

---
+++ JavaScript/chapter_02/program_0222/app.js
@@ -0,0 +1,17 @@
+// This program displays the following
+// floating-point numbers in a column
+// with their decimal point aligned.
+var num1 = 127.899;
+var num2 = 3465.148;
+var num3 = 3.776;
+var num4 = 264.821;
+var num5 = 88.081;
+var num6 = 799.999;
+
+// Display each number with 2 decimal places.
+console.log(num1.toFixed(2));
+console.log(num2.toFixed(2));
+console.log(num3.toFixed(2));
+console.log(num4.toFixed(2));
+console.log(num5.toFixed(2));
+console.log(num6.toFixed(2));
