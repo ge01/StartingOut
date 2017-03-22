f76360e936fc4723568d17d99cc882ae436be5fa
Gabe Escobedo <gabriel.escobedo01@utrgv.edu>
2017-03-22 09:05:41 -0500

    JS - the main function calls the message function

---
+++ JavaScript/chapter_03/program_0302/program_0302/JavaScript.js
@@ -0,0 +1,16 @@
+﻿// This program has two functions. First we
+// define the main function.
+function main() {
+    console.log("I have a message for you.");
+    message();
+    console.log("Goodbye!");
+}
+
+// Next we define the message function.
+function message() {
+    console.log("I am Arthur,");
+    console.log("King of the Britons.");
+}
+
+// Call the main function.
+main();
