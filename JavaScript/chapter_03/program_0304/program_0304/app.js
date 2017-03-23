45461aab14c39952b09fe3c32dd3c3bae36653a3
Gabe Escobedo <gabriel.escobedo01@utrgv.edu>
2017-03-23 04:40:02 -0500

    JS - a local variable is created inside a function and cannot be accessed by statements that are outside the function

---
+++ JavaScript/chapter_03/program_0304/program_0304/app.js
@@ -0,0 +1,14 @@
+﻿// Definition of the main function.
+function main() {
+    getName()
+    print('Hello', name)    // This causes an error!
+}
+
+
+// Definition to the getName function.
+function getName() {
+    name = input('Enter your name: ')
+}
+ 
+// Call the main function.
+main()
