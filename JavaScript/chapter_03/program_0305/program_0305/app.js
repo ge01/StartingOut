174203c4961a11bf3375fca1aa429071706c4e84
Gabe Escobedo <gabriel.escobedo01@utrgv.edu>
2017-03-23 05:09:54 -0500

    JS - two functions that have local variables with the same name

---
+++ JavaScript/chapter_03/program_0305/program_0305/app.js
@@ -0,0 +1,26 @@
+﻿// This program demonstrates two functions that
+// have local variables with the same name.
+
+function main() {
+    // Call the texas function.
+    texas()
+    // Call the california function.
+    california()
+}
+
+// Definition of the texas function. It creates
+// a local variable named birds.
+function texas() {
+    birds = 5000;
+    console.log('texas has', birds, 'birds.');
+}
+
+// Definition of the california function. It also
+// creates a local variable named birds.
+function california() {
+    birds = 8000;
+    console.log('california has', birds, 'birds.');
+}
+
+// Call the main function.
+main();
