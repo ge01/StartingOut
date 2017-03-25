9a2e2d5c18ddbdee010f8ce3cccdc9c91834c53e
Gabe Escobedo <gabriel.escobedo01@utrgv.edu>
2017-03-25 07:32:38 -0500

    JS - demontrate an argument being passed to a function

---
+++ JavaScript/chapter_03/program_0306/program_0306/app.js
@@ -0,0 +1,19 @@
+﻿// This program demonstrates an argument being
+// passed to a function.
+
+function main() {
+    var value = 5;
+    showDouble(value);
+}
+
+
+// The show_double function accepts an argument
+// and displays double its value.
+function showDouble(number) {
+    result = number * 2;
+    console.log(result);
+}
+
+
+// Call the main function.
+main();
