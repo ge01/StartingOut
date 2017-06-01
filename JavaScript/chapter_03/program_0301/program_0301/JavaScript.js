0f930d0f4a5498c80f258364f7f2a555fba62fa1
ge01 <gabriel.escobedo01@utrgv.edu>
2017-06-01 16:06:24 -0500

    JS - calling a function

---
+++ JavaScript/chapter_03/program_0301/program_0301/JavaScript.js
@@ -0,0 +1,9 @@
+﻿// This program demonstrates a function.
+// First, we define a function named message.
+function message() {
+    console.log("I am Arthur,");
+    console.log("King of the Britons.")
+}
+
+// Call the message function.
+message();
