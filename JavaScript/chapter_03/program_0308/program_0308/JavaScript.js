d95e6f308b5fe6da3dbb36ca8bba245df5bfe553
ge01 <gabriel.escobedo01@utrgv.edu>
2017-06-18 21:43:21 -0500

    JS - demonstrates arrow function that accepts two arguments

---
+++ JavaScript/chapter_03/program_0308/program_0308/JavaScript.js
@@ -0,0 +1,28 @@
+﻿// This program demonstrates a function that accepts
+// two arguments.
+
+var main = () => {
+    console.log('The sum of 12 and 45 is')
+    show_sum(12, 45)
+}
+
+// The show_sum function accepts two arguments
+// and displays their sum.
+var show_sum = (num1, num2) => {
+    result = num1 + num2;
+    console.log(result);
+}
+
+// Call the main function
+main();
+
+
+/*  Guide for using arrow functions
+let addOne = function (value) {
+    return value + 1;
+}
+
+let addOne = (value) => {
+    return value + 1;
+}
+*/
