e4d175b4e9876630133d691310a048d6ebd00a38
ge01 <gabriel.escobedo01@utrgv.edu>
2017-06-18 22:41:25 -0500

    C# - this program demonstrates a function

---
+++ CSharp/chapter_03/program_0301/program_0301/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program_0301
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This program demonstrates a function.
+            // First, we define a function named message.
+            void message()
+            {
+                Console.WriteLine("I am Arthur,");
+                Console.WriteLine("King of the Britons.");
+                Console.ReadLine();
+            }
+
+            // Call the message function.
+            message();
+        }
+    }
+}
