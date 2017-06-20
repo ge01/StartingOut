ef4467c73ea316c3555d33444f6ff55ebeb3702f
ge01 <gabriel.escobedo01@utrgv.edu>
2017-06-19 20:47:07 -0500

    JS - demonstrates passing two string arguments to a function

---
+++ JavaScript/chapter_03/program_0309/program_0309/JavaScript.js
@@ -0,0 +1,16 @@
+﻿// This program demonstrates passing two string
+// arguments to a function.
+
+function main() {
+   first_name = prompt('Enter your first name: ');
+   last_name = prompt('Enter you last name: ');
+   console.log('Your name reversed is ');
+   reverse_name(first_name, last_name);
+}
+
+reverse_name = (first, last) => {
+    console.log(last, first)
+}
+
+// Call the main function
+main();
