387d4af9bbdb8352ab31718cd8ce34400d379c27
ge01 <gabriel.escobedo01@utrgv.edu>
2017-06-19 21:11:42 -0500

    C# - in OOP a method is a subroutine (or procedure or function)

---
+++ CSharp/chapter_03/program_0302/program_0302/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program_0302
+{
+    class Program
+    {
+        // This program has two functions. First we
+        // define the main function.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I have a message for you.");
+            message();
+            Console.WriteLine("Goodbye!");
+            Console.ReadLine();
+        }
+
+        // Next we define the message function.
+        static void message()
+        {
+            Console.WriteLine("I am Arthur,");
+            Console.WriteLine("King of the Britons.");
+        }        
+    }
+}
