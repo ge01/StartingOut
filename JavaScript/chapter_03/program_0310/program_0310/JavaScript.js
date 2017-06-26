2aab020f3417642c1e5fc2c6ac430d4a5dbbf5c4
ge01 <gabriel.escobedo01@utrgv.edu>
2017-06-25 20:33:00 -0500

    JS - demonstrates what happens when value of a parameter is changed.

---
+++ JavaScript/chapter_03/program_0310/program_0310/JavaScript.js
@@ -0,0 +1,18 @@
+﻿// This program demonstrates what happens when you 
+// change the value of a parameter.
+
+function main() {
+    var value = 99;
+    console.log('The value is ', value);
+    change_me(value);
+    console.log('Back in main the value is ', value);
+}
+
+change_me = (arg) => {
+    console.log('I am changing the value.');
+    arg = 0;
+    console.log('Now the value is', arg)
+}
+
+// Call the main function.
+main();
