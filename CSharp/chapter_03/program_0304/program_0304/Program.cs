025d4cdec2923597877a7f9bad6524ee1d15e07e
ge01 <gabriel.escobedo01@utrgv.edu>
2017-06-30 21:25:38 -0500

    C# - error a statement in one function tries to access a local variable that belongs to another function.

---
+++ CSharp/chapter_03/program_0304/program_0304/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program_0304
+{
+    class Program
+    {
+        // Definition of the main function.
+        static void Main(string[] args)
+        {
+            get_name();
+            Console.WriteLine("Hello", name);   // This causes an error
+        }
+
+        // Definition of the get_name function.
+        static void get_name()
+        {
+            Console.WriteLine("Enter your name: ");
+            var name = Console.ReadLine();
+        }
+    }
+}
