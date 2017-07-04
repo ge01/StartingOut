34a5029a528088d47aa397aa27d9455e6bdf8d98
ge01 <gabriel.escobedo01@utrgv.edu>
2017-07-03 21:55:53 -0500

    C# - demonstrates two functions that have local variables with the same name

---
+++ CSharp/chapter_03/program_0305/program_0305/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program_0305
+{
+    class Program
+    {
+        // This program demonstrates two functions that
+        // have local variables with the same name.
+        static void Main(string[] args)
+        {
+            // Call the texas function.
+            texas();
+
+            // Cal the california function.
+            california();
+
+            Console.ReadLine();
+        }
+
+        // Definition of the texas function. It creates
+        // a local variable named birds.
+        static void texas()
+        {
+            int birds = 5000;
+            Console.WriteLine("texas has " + birds + " birds.");
+        }
+
+        // Definition of the california function. It also
+        // creates a local variable named birds.
+        static void california()
+        {
+            int birds = 8000;
+            Console.WriteLine($"california has {birds} birds.");
+        }        
+    }
+}
