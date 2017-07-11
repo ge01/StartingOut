dbfc73eca74cb38b819b3267894e81abf9935c20
ge01 <gabriel.escobedo01@utrgv.edu>
2017-07-10 23:53:53 -0500

    JS - kilometer converter

---
+++ JavaScript/chapter_03/programming_exercises/pe_0301/pe_0301/app.js
@@ -0,0 +1,30 @@
+﻿var CONSTANT = 0.6214;
+
+function main() {
+    // Display the intro screen.
+    intro();
+
+    // Get the distance in kilometers.
+    var kilometers;
+    kilometers = parseFloat(prompt("Enter the distance in kilometers: "));
+
+    // Convert the kilometers to miles.
+    kilometers_to_miles(kilometers);
+}
+
+// The intro function displays an introductory screen.
+function intro() {
+    console.log('This program converts distance to miles.');
+    console.log('');
+}
+
+// The kilometers_to_miles function accepts a number in 
+// kilometers and displays the equivalent number in miles.
+let kilometers_to_miles = (kilos) => {
+    miles = kilos * CONSTANT;
+    console.log('That converts to '
+                 + miles.toFixed(2));
+}
+
+// Call the main function
+main();
