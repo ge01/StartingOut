fed0c33a1f9e6436a0be5e9b81cdde2f99260c21
ge01 <gabriel.escobedo01@utrgv.edu>
2017-07-11 23:32:42 -0500

    C# - demonstrating an argument being passed to a function

---
+++ CSharp/chapter_03/program_0306/program_0306/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program_0306
+{
+    class Program
+    {
+        // This program demonstrates an argument being 
+        // passed to a function
+        static void Main(string[] args)
+        {
+            var value = 5;
+            show_double(value);
+            Console.ReadLine();
+        }
+
+        // The show_double function accepts an argument
+        // and displays double its value.
+        static void show_double(int number)
+        {
+            var result = number * 2;
+            Console.WriteLine(result);
+        }
+    }
+}
