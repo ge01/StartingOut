7e58e2f1f63488d1f02c2fea81e6f7f159cfdb05
ge01 <gabriel.escobedo01@utrgv.edu>
2017-07-23 21:24:01 -0500

    C# - this program demonstrates a function that accepts two arguments

---
+++ CSharp/chapter_03/program_0308/program_0308/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program_0308
+{
+    class Program
+    {
+        // This program demonstrates a function that accepts 
+        // two arguments.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The sum of 12 and 45 is");
+            show_sum(12, 45);
+            Console.ReadLine();
+        }
+
+        // The show_sum function accepts two arguments
+        // and displays their sum.
+        static void show_sum(int num1, int num2)
+        {
+            var result = num1 + num2;
+            Console.WriteLine(result);
+        }
+    }
+}
