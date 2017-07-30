3aa39b6b461fbd16cdc6291d258265d98e3a2d06
ge01 <gabriel.escobedo01@utrgv.edu>
2017-07-29 22:53:01 -0500

    C# - passing two string arguments to a functon

---
+++ CSharp/chapter_03/program_0309/program_0309/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program_0309
+{
+    class Program
+    {
+        // This program demonstrates passing two string
+        // arguments to a function.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your first name: ");
+            var first_name = Console.ReadLine();
+
+            Console.WriteLine("Enter your last name: ");
+            var last_name = Console.ReadLine();
+
+            reverse_name(first_name, last_name);
+            Console.ReadLine();
+        }
+
+        static void reverse_name(string first, string last)
+        {
+            Console.WriteLine("{0}, {1}", last, first);
+        }
+    }
+}
