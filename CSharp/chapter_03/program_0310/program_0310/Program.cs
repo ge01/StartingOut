a8f3088c5b0b26c113675af34bb753e13a5cfcde
ge01 <gabriel.escobedo01@utrgv.edu>
2017-08-05 23:17:18 -0500

    C# - making changes to parameters

---
+++ CSharp/chapter_03/program_0310/program_0310/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program_0310
+{
+    class Program
+    {   
+        // This program demonstrates what happens when you 
+        // change the value of a parameter.
+        static void Main(string[] args)
+        {
+            var value = 99;
+            Console.WriteLine("The value is {0}", value);
+            change_me(value);
+            Console.WriteLine("Back in main the value {0}", value);
+            Console.ReadLine();
+        }
+
+        static void change_me(int arg)
+        {
+            Console.WriteLine("I am changing the value");
+            arg = 0;
+            Console.WriteLine($"Now the value is {arg}");
+        }
+    }
+}
