07b10f3e0ea02f82f3d195f5b4d4ebf43e289593
ge01 <gabriel.escobedo01@utrgv.edu>
2017-10-15 19:43:55 -0500

    this program uses E notation

---
+++ Java/chapter_02/program_0212/src/program_0212.java
@@ -0,0 +1,16 @@
+// This program uses E notation.
+
+public class program_0212
+{
+    public static void main(String[] args)
+    {
+        double distance, mass;
+
+        distance = 1.495979E11;
+        mass = 1.495979E11;
+        System.out.println("The sum is " + distance +
+                           " meters away.");
+        System.out.println("The sun's mass is " + mass +
+                           " kilograms.");
+    }
+}
