3cb2b8f1c8caa4cfed8c7d0d8bf61191ae3fbd5a
ge01 <gabriel.escobedo01@utrgv.edu>
2017-10-17 00:53:15 -0500

    a progam for demonstrating boolean variables

---
+++ Java/chapter_02/program_0213/src/program_0213.java
@@ -0,0 +1,14 @@
+// A program for demonstrating boolean variables
+
+public class program_0213
+{
+    public static void main(String[] args)
+    {
+        boolean bool;
+
+        bool = true;
+        System.out.println(bool);
+        bool = false;
+        System.out.println(bool);
+    }
+}
