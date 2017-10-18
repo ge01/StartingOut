e7900dae9a003fc0d642eb6921dc97a9f3a1fb0d
ge01 <gabriel.escobedo01@utrgv.edu>
2017-10-17 19:49:51 -0500

    this program demonstrates the char data type

---
+++ Java/chapter_02/program_0214/src/code_0214.java
@@ -0,0 +1,14 @@
+// This program demonstrates the char data type.
+
+public class code_0214
+{
+    public static void main(String[] args)
+    {
+        char letter;
+
+        letter = 'A';
+        System.out.println(letter);
+        letter = 'B';
+        System.out.println(letter);
+    }
+}
